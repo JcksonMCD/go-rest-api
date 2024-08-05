80d2bb976048d801927a34d150eed3374fcbf138
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-08-05 15:18:09 +0100

    Feat: Endpoint method set up for get book by id.

--- main.go
+++ main.go
@@ -26,6 +26,17 @@ func getBooks(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, books)
 }
 
+func bookById(c *gin.Context) {
+	id := c.Param("id")
+	book, err := getBookById(id)
+
+	if err != nil {
+		return
+	}
+
+	c.IndentedJSON(http.StatusOK, book)
+}
+
 func getBookById(id string) (*book, error) {
 	for i, b := range books {
 		if b.ID == id {
