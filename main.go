3831dac702ed1305317d64cd051a0136760a5444
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-08-05 14:55:50 +0100

    Feat: create book method created to be used for a post endpoint.

--- main.go
+++ main.go
@@ -25,7 +25,20 @@ func getBooks(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, books)
 }
 
+func createBook(c *gin.Context) {
+	var newBook book
+
+	if err := c.BindJSON(&newBook); err != nil {
+		// BindJSON will handlesending error response so empty return can be used.
+		return
+	}
+
+	books = append(books, newBook)
+	c.IndentedJSON(http.StatusCreated, newBook)
+}
+
 func main() {
 	router := gin.Default()
 	router.GET("/books", getBooks)
+	router.Run("localhost:8080")
 }
