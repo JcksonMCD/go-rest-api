112875dbe15c38cef9c3aa988b6a472462f8a082
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-08-05 14:28:48 +0100

    Feat: Main func set up and gin router created.

--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
+import "github.com/gin-gonic/gin"
+
 type book struct {
 	// Capital field names allow for exportation.
 	ID       string `json:"id"`
@@ -13,4 +15,8 @@ var books = []book{
 	{ID: "1", Title: "In Search of Lost Time", Author: "Marcel Proust", Quantity: 2},
 	{ID: "2", Title: "The Great Gatsby", Author: "F. Scott Fitzgerald", Quantity: 5},
 	{ID: "3", Title: "War and Peace", Author: "Leo Tolstoy", Quantity: 6},
+}
+
+func main() {
+	router := gin.Default()
 }
