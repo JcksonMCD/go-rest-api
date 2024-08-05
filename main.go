7f66b7cd08df17d1d977411c896e6854c9724182
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-08-05 14:25:43 +0100

    Feat: Slice of books created to simulate a database connection.

--- main.go
+++ main.go
@@ -1,8 +1,16 @@
 package main
 
-type Book struct {
+type book struct {
+	// Capital field names allow for exportation.
 	ID       string `json:"id"`
 	Title    string `json:"title"`
 	Author   string `json:"author"`
 	Quantity int    `json:"quantity"`
+}
+
+// Data for API as not connecting to a db for this api.
+var books = []book{
+	{ID: "1", Title: "In Search of Lost Time", Author: "Marcel Proust", Quantity: 2},
+	{ID: "2", Title: "The Great Gatsby", Author: "F. Scott Fitzgerald", Quantity: 5},
+	{ID: "3", Title: "War and Peace", Author: "Leo Tolstoy", Quantity: 6},
 }
