1f934b192084a18f157ff780f13e980597490c99
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-08-06 10:07:52 +0100

    Feat: Further assertions added to test case to check body response.

--- main_test.go
+++ main_test.go
@@ -30,4 +30,11 @@ func TestGetBooks(t *testing.T) {
 	if w.Code != http.StatusOK {
 		t.Errorf("Expected status code %d, but got %d", http.StatusOK, w.Code)
 	}
+
+	// Assert that the response body is as expected
+	expected := `[{"id":"1","title":"In Search of Lost Time","author":"Marcel Proust","quantity":2},{"id":"2","title":"The Great Gatsby","author":"F. Scott Fitzgerald","quantity":5},{"id":"3","title":"War and Peace","author":"Leo Tolstoy","quantity":6}]
+		`
+	if w.Body.String() != expected {
+		t.Errorf("Expected body %s, but got %s", expected, w.Body.String())
+	}
 }
